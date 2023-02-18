0a8b367950b8880aebb40a0cb191a2ab86036250
Caden DeGlopper <caden.deglopper@gmail.com>
2023-02-18 10:47:15 -0600

    Temporary turned off pids

--- src/main/java/frc/robot/subsystems/elevator/ElevatorIORobot.java
+++ src/main/java/frc/robot/subsystems/elevator/ElevatorIORobot.java
@@ -2,6 +2,7 @@ package frc.robot.subsystems.elevator;
 
 import com.revrobotics.CANSparkMax;
 import com.revrobotics.RelativeEncoder;
+import com.revrobotics.CANSparkMax.IdleMode;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 
 import edu.wpi.first.math.util.Units;
@@ -23,6 +24,7 @@ public class ElevatorIORobot implements ElevatorIO {
     
         //LeftElevatorFollower.restoreFactoryDefaults();
         rightElevatorMaster.restoreFactoryDefaults();
+        rightElevatorMaster.setIdleMode(IdleMode.kBrake);
 
         rightElevatorEncoder.setPositionConversionFactor(circumference / gearRatio);
         //LeftElevatorFollower.follow(RightElevatorMaster, true);
