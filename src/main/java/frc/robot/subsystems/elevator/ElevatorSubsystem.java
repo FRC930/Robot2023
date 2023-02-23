42dfbad4a48865b507264d27855f3bfbf92dde24
Nolan655 <gooddevil987666@gmail.com>
2023-02-22 20:55:00 -0600

    Added some of gregs Sim code to have the elevator move in sim
    2/22/23
    8:54 pm

--- src/main/java/frc/robot/subsystems/elevator/ElevatorSubsystem.java
+++ src/main/java/frc/robot/subsystems/elevator/ElevatorSubsystem.java
@@ -6,6 +6,8 @@ import edu.wpi.first.math.controller.ProfiledPIDController;
 import edu.wpi.first.math.trajectory.TrapezoidProfile.Constraints;
 import edu.wpi.first.math.util.Units;
 import edu.wpi.first.wpilibj.DriverStation;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Robot;
@@ -35,8 +37,8 @@ public class ElevatorSubsystem extends SubsystemBase{
         //our p is in terms of meters, meaning you are multiplying a decmal by p
         this.controller = new ProfiledPIDController(45, 0, 0, 
                  new Constraints(Units.inchesToMeters(36), Units.inchesToMeters(30))); //This is in meters
+       
         this.ff = new ElevatorFeedforward(0, 0.45, 0, 0);
-
         this.topff = new ElevatorFeedforward(0, 0.45, 0, 0);
 
 
@@ -66,10 +68,9 @@ public class ElevatorSubsystem extends SubsystemBase{
      */
     @Override
     public void periodic() {
-
+        m_io.updateInputs();
         //Checks if the robot is a simulation
         if (DriverStation.isEnabled() || !Robot.isReal()) {
-            m_io.updateInputs();
             double currentheight = m_io.getCurrentHeight();
 
             double voltage = controller.calculate(currentheight, targetElevatorPosition);
@@ -92,4 +93,14 @@ public class ElevatorSubsystem extends SubsystemBase{
         SmartDashboard.putNumber(this.getClass().getSimpleName()+"/EncoderValue", getElevatorPosition());
         SmartDashboard.putNumber(this.getClass().getSimpleName()+"/EncoderValue(Inches)", Units.metersToInches(getElevatorPosition()));
     }
+    private Boolean pastHeight(double meters) {
+        double height = getElevatorPosition();
+        System.out.println("height: " + height + " target: " + meters);
+        return height > meters;
+    }
+
+    public Command waitUntilPastHeightCommand(double meters) {
+        return Commands.waitUntil(() -> this.pastHeight(meters));
+    }
+
 }
