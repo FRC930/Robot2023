a1f837d5c0c2fc57132ca8b3c97efd460fdd0f53
swegner <steve.wegner@gmail.com>
2023-03-05 17:26:19 -0600

    Adjust Upright cone angles

--- src/main/java/frc/robot/utilities/CommandFactoryUtility.java
+++ src/main/java/frc/robot/utilities/CommandFactoryUtility.java
@@ -35,7 +35,7 @@ public class CommandFactoryUtility {
 
     // Arm Intake UpRight cone
     private static final double ELEVATOR_UPRIGHT_INTAKE_HEIGHT = 17.4 * FACTOR; // 1.28/1.756  ;
-    private static final double ARM_UPRIGHT_INTAKE_ANGLE = -15.0;
+    private static final double ARM_UPRIGHT_INTAKE_ANGLE = -13.5;
     private static final double MANIPULATOR_UPRIGHT_INTAKE = 8.5;
 
     // Arm Back intake DONT USE
