0df4bd2bb1813e7d39341624210220f54287bdd7
swegner <steve.wegner@gmail.com>
2023-03-05 16:50:15 -0600

    MidScore adjustment Comment out y() in telelopSwerve (DID NOT WANT TO INTERFERE WITH real Y BUTTON) reduce release speed change low score (to be close to stow position)

--- src/main/java/frc/robot/commands/TeleopSwerve.java
+++ src/main/java/frc/robot/commands/TeleopSwerve.java
@@ -70,12 +70,13 @@ public class TeleopSwerve extends CommandBase {
         // Gets the inputs from the joysticks
         double yAxis = -m_controller.getHID().getRawAxis(m_translationAxis);
         double xAxis = -m_controller.getHID().getRawAxis(m_strafeAxis);
-        if (m_controller.getHID().getYButton()) {
-            rAxis = m_rotationMathUtility.rotationSpeed(m_Swerve.getPose(), m_rotatePositions.getPose2dForRotation(), m_Swerve.getHeadingDegrees());
-        }
-        else{
+        // TODO REVIEW BEFORE ENABLING SINCE Y() was used by drive to release cone/cube
+        // if (m_controller.getHID().getYButton()) {
+        //     rAxis = m_rotationMathUtility.rotationSpeed(m_Swerve.getPose(), m_rotatePositions.getPose2dForRotation(), m_Swerve.getHeadingDegrees());
+        // }
+        // else{
             rAxis = -m_controller.getHID().getRawAxis(m_rotationAxis);
-        }
+        // }
 
         // Applies a deadband to the values
         yAxis = MathUtil.applyDeadband(yAxis, STICK_DEAD_BAND);
--- src/main/java/frc/robot/utilities/CommandFactoryUtility.java
+++ src/main/java/frc/robot/utilities/CommandFactoryUtility.java
@@ -25,31 +25,39 @@ import frc.robot.subsystems.manipulator.ManipulatorSubsystem;
 
 public class CommandFactoryUtility {
 
+    // Conversion factor for Elevator (pulley sizes changed)
     private static double FACTOR = .68;
+
+    //Arm Intake ground/low
     private static final double ELEVATOR_INTAKE_HEIGHT = 12.0 * FACTOR; // 1.28/1.756  ;
     private static final double ARM_INTAKE_ANGLE = -24.0;
     private static final double MANIPULATOR_INTAKE = 17.0;
 
+    // Arm Intake UpRIght cone
     private static final double ELEVATOR_UPRIGHT_INTAKE_HEIGHT = 17.4 * FACTOR; // 1.28/1.756  ;
     private static final double ARM_UPRIGHT_INTAKE_ANGLE = -22.0;
     private static final double MANIPULATOR_UPRIGHT_INTAKE = 4.5;
 
+    // Arm Back intake DONT USE
     // TODO DONT USE YET WRIST WILL CRASH INTO ARM (need to find way to move safely)
     private static final double ELEVATOR_BACK_INTAKE_HEIGHT = 14.0 * FACTOR; //not sure if correct?
     private static final double ARM_BACK_INTAKE_ANGLE = 184.5;
     private static final double MANIPULATOR_BACK_INTAKE = 260.0;
 
+    // High Score
     private static final double ELEVATOR_HIGH_SCORE_HEIGHT =  50.0 * FACTOR; // 1.28/1.756  ;
     private static final double ARM_HIGH_SCORE_ANGLE = 55.0; 
     private static final double MANIPULATOR_HIGH_SCORE = -3.0;
 
+    // Mid Score
     private static final double ELEVATOR_MID_SCORE_HEIGHT =  20.0  * FACTOR; // 1.28/1.756  ;
-    private static final double ARM_MID_SCORE_ANGLE = 55.0; 
+    private static final double ARM_MID_SCORE_ANGLE = 58.0; 
     private static final double MANIPULATOR_MID_SCORE = -3.0;
 
-    private static final double ELEVATOR_LOW_SCORE_HEIGHT = 12.0 * FACTOR; // 1.28/1.756  ;
-    private static final double ARM_LOW_SCORE_ANGLE = -25.0;
-    private static final double MANIPULATOR_LOW_SCORE = 25.0;
+    // Low Score
+    private static final double ELEVATOR_LOW_SCORE_HEIGHT = 0.0 * FACTOR; // 1.28/1.756  ;
+    private static final double ARM_LOW_SCORE_ANGLE = 70.0;
+    private static final double MANIPULATOR_LOW_SCORE = -5.0;
 
 
     private CommandFactoryUtility() {}
