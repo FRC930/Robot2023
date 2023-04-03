e360b778f3020d8daf6c3e16b8c9ee37e1b305b3
Harry <96154793+WasteOfTheOcean@users.noreply.github.com>
2023-04-03 18:27:20 -0500

    Changed Manipulator Roller Constants for New Gear Ratio 18:27 Only validated double substation so far.

--- src/main/java/frc/robot/subsystems/manipulator/ManipulatorSubsystem.java
+++ src/main/java/frc/robot/subsystems/manipulator/ManipulatorSubsystem.java
@@ -29,10 +29,10 @@ public class ManipulatorSubsystem extends SubsystemBase {
     public static double INTAKE_POSITION = CommandFactoryUtility.MANIPULATOR_INTAKE; // low intake Position
     public static final double SUBSTATION_POSITION = CommandFactoryUtility.MANIPULATOR_SUBSTATION;//-125; want position to force long way if continuousinput commented out
 
-    public static final double ROLLER_INTAKE_SPEED = 0.8;
+    public static final double ROLLER_INTAKE_SPEED = 1.0;
     public static final double DOUBLE_SUBSTATION_ROLLER_INTAKE_SPEED = 1.0;
     public static final double SHOOT_SPEED = -1.0;
-    public static final double RELEASE_SPEED = -0.35;
+    public static final double RELEASE_SPEED = -0.7;
     public static final double HOLD_SPEED = 0.15;
    
 
