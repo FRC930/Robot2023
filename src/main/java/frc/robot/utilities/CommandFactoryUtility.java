949f7cf16beedd6c2b05849b91df577bd926950f
Jack Schumacher <84467112+jackrschumacher11@users.noreply.github.com>
2023-04-25 20:26:54 -0500

    (20:26 4/25) Update Arm Intake Angle for Demo
    Changed the Arm intake Angle for Demo. Changed from -24 to -29 degrees.

--- src/main/java/frc/robot/utilities/CommandFactoryUtility.java
+++ src/main/java/frc/robot/utilities/CommandFactoryUtility.java
@@ -27,7 +27,7 @@ public class CommandFactoryUtility {
 
     //Arm Intake ground/low
     public static final double ELEVATOR_INTAKE_HEIGHT = 12.0 * FACTOR; // 1.28/1.756  ;
-    public static final double ARM_INTAKE_ANGLE = -24.0;
+    public static final double ARM_INTAKE_ANGLE = -29.0;
     public static final double MANIPULATOR_INTAKE = 17.0 + MANIPULATOR_REDUCTION - 4;
 
     // Extend Ground Intake
